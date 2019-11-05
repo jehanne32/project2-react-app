cd887fb9b8d110d519e7abaf849248ab7b5dc21c
Michelle Jensen <mchlljnsn@yahoo.com>
2019-11-05 12:21:10 -0500

    WIP third commit

--- src/MovieTitle.js
+++ src/MovieTitle.js
@@ -7,7 +7,7 @@ class MovieTitle extends Component {
       movieTitle: [],
       userentry: []
     };
-    // console.log('movietitle props', props)
+    
   }
 
   submitHandler = event => {
@@ -17,39 +17,42 @@ class MovieTitle extends Component {
 
   changeHandler = event => {
     this.setState({ userentry: event.target.value });
-    //   const movie = document.querySelector(".titleInput")
-    // this.setState({ movieTitle: movie.value });
-    //console.log(movie.value);
+  
   };
 
   render() {
     let title;
     let overview;
+    let release_date;
+    let backdrop_path;
+    let poster_path;
 
     if (this.props.movies.length > 0) {
       console.log(this.props.movies[0])
       title = this.props.movies[0].title
       overview = this.props.movies[0].overview
+      release_date = this.props.movies[0].release_date
+      backdrop_path = `https://image.tmdb.org/t/p/w500/${this.props.movies[0].backdrop_path}`
+      poster_path = `https://image.tmdb.org/t/p/w500/${this.props.movies[0].poster_path}`
 
     }
-
-    // <p key={index}>{movie.title}</p>
 
     return (
       <div>
         <h2>Movie Title</h2>
         <form onSubmit={this.submitHandler}>
           <p>Enter a movie title and click search</p>
-          {/* <input className="titleInput" type="text" /> */}
           <input type="text" name="title" onChange={this.changeHandler} />
-          {/* <button onClick={(e) => this.changeHandler(e)} >Search</button> */}
+          
           <input type="submit" />
         </form>
 
         <div>
           <p>{title}</p>
-        
+          <p>{release_date}</p>
           <p>{overview}</p>
+          <img src={backdrop_path}/>
+          <img src={poster_path}/>
         </div>
       </div>
     );
